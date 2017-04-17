3ff5ed0a289ba5b0df2603f2c62eaeee3fb5a7cd
Bingo <bentleyjobs@gmail.com>
2017-04-18 01:55:13 +0800

    新增ipcClient实现

--- test/ipc.h
+++ test/ipc.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <core/messageQueue/a3Message.h>
+#include <core/messageQueue/a3MessageQueueIPC.h>
+
+void main()
+{
+    a3MessageQueueIPC ipc;
+
+    ipc.init(L"Who's Your Daddy", false, 100, 1024);
+
+    for(int i = 0; i < 100; i++)
+    {
+        c2sGridImageMessage msg;
+        msg.shit = i;
+        ipc.enqueue(msg);
+    }
+
+    getchar();
+}
